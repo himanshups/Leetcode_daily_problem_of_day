dc52c9684bcbbe8b712912bf70d5ac937aec3b12
himanshups <74396707+himanshups@users.noreply.github.com>
2024-07-01 20:29:03 +0530

    Create Day_67_Three Consecutive Odds.cpp

---
+++ Day_67_Three Consecutive Odds.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        for(int i = 0;i<n-2;i++){
+            if((arr[i]*arr[i+1]*arr[i+2])%2==1) 
+                return true;
+        }
+        return false;
+    }
+};
