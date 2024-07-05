27e46748c2e29bf0d6270ca775693c6e2f0ea722
himanshups <74396707+himanshups@users.noreply.github.com>
2024-07-05 21:03:56 +0530

    Create Day_71_Find the minimum and maximum number of nodes between critical points.cpp

---
+++ Day_71_Find the minimum and maximum number of nodes between critical points.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        int firstCriticalIndex    = 0;
+        int previousCriticalIndex = 0;
+
+        int prevValue = 0;
+        int currValue = 0;
+        int nextValue = 0;
+
+        int minDistance = INT_MAX;
+        int index = 0;
+        vector<int> result = {-1, -1};
+
+        while (head != NULL) {
+            prevValue = currValue;
+            currValue = nextValue;
+            nextValue = head->val;
+
+            if (prevValue != 0 && currValue != 0 && nextValue != 0 && 
+                (prevValue > currValue && currValue < nextValue ||
+                prevValue < currValue && currValue > nextValue)) {
+                
+                if (firstCriticalIndex == 0) {
+                    firstCriticalIndex = index;
+                } else {
+                    minDistance = min(minDistance, index - previousCriticalIndex);
+                    result = {minDistance, index - firstCriticalIndex};
+                }
+                previousCriticalIndex = index;
+            }
+
+            index++;
+            head = head->next;
+        }
+
+        return result;        
+    }
+};
